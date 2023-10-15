84c0ee597489c760b66805b5831253fd77d9f545
PikaBeka <beka.kalmahanbet@gmail.com>
2023-10-15 17:32:32 +0600

    feat(count): change points and ips for shymkent

--- config.py
+++ config.py
@@ -1,25 +1,15 @@
 from utils.draw_utils import Point
 
 # THe first camera settings
-# IP1 = "192.168.0.92"  # "5:554"
-# NAME1 = "admin"
-# PSWD1 = "The2ndlaw"
-
-IP1 = "192.168.0.2"  # "5:554"
+IP1 = "192.168.0.92"  # "5:554"
 NAME1 = "admin"
-PSWD1 = "james1989"
+PSWD1 = "The2ndlaw"
 
-# LINE_START1 = Point(350, 800)
-# LINE_END1 = Point(1650, 800)
 LINE_START1 = Point(0, 225)
 LINE_END1 = Point(1920, 225)
 
 # The second camera settings
-# IP2 = "192.168.0.4:554"
-# NAME2 = "admin"
-# PSWD2 = "james1989"
-
-IP2 = "192.168.0.2"  # "5:554"
+IP2 = "192.168.0.4:554"
 NAME2 = "admin"
 PSWD2 = "james1989"
 
